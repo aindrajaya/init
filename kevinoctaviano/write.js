a6742ba825478d6dbd4d03b59f9555ed0835cc67
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-03-14 21:22:57 +0700

    update writefile

--- kevinoctaviano/write.js
+++ kevinoctaviano/write.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 
 // isi konten
-const content = 'Ini konten Kevin Octaviano';
-const filePath = './content.txt';
+const html = `<!DOCTYPE html>
+<html lang="en">
 
-fs.writeFileSync(filePath, content, 'utf-8');
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello, Kevin Octaviano!</h1>
+</body>
+
+</html>`;
+const filePath = './index.html';
+
+fs.writeFileSync(filePath, html, 'utf-8');
 
 
