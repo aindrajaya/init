e3b4e4f35e89da512767c2685afe8f6f30e8eb9c
Faisal <karimfaisal3452@gmail.com>
2022-03-14 21:11:36 +0700

    commit

---
+++ faisal/write.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+//Penamaan variabel untuk js biasanya pakai camelCase
+const isiKonten =
+    `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    
+    <body>
+        <h2>hello world</h2>
+    </body>
+    
+    </html>
+    `;
+
+fs.writeFileSync('coba.html', isiKonten, function(err) {
+    if (err) throw err;
+    console.log('File html Created');
+})
