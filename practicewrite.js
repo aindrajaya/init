a07cfaadd8f7a90b0616ad9e64b380101edec267
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-03-14 21:25:50 +0700

    write the html file

--- practicewrite.js
+++ practicewrite.js
@@ -1,6 +1,24 @@
 const fs = require("fs");
 
-fs.writeFile("test.txt", "This is the text", (err) => {
-    if (err) throw err;
-    console.log("Write Complete");
-});
+fs.writeFile(
+    "test.html",
+    `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="button"></div>
+</body>
+
+</html>`,
+    (err) => {
+        if (err) throw err;
+        console.log("Write Complete");
+    }
+);
