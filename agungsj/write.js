ac92bf82856b5c70a9bf6fa047d77456c9b4ceda
Agung Sukrisna Jaya <agungsmk02@gmail.com>
2022-03-14 20:59:07 +0700

    commit read dan write asj

---
+++ agungsj/write.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+
+const isikonten = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> hello world </h1>
+    <h1> hello world dari index.js
+</body>
+</html>`;
+
+fs.writeFile('coba.html', isikonten, function(err){
+
+  if(err) throw err;
+  console.log('File Created');
+});
