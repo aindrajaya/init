e387ba346a25c05b10019318170dbb7787e89ad5
alfajri23 <123190088@student.upnyk.ac.id>
2022-03-15 21:29:22 +0700

    eslint

---
+++ feri/read.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+// read file sample.html
+fs.readFile('./html.html',
+	// callback function that is called when reading file is done
+	function(err, data) {		
+		if (err) throw err;
+		// data is a buffer containing file content
+		console.log(data.toString('utf8'))
+});
