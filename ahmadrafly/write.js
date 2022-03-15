7823dcbadf7b86689d8d0ebe577b1383224ee2b4
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2022-03-15 19:37:26 +0700

    update konten html

--- ahmadrafly/write.js
+++ ahmadrafly/write.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
 
-const isiKonten = "Learn Node Js";
+const isiKonten = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body></body>
+</html>`;
 
-fs.writeFile("write.txt", isiKonten, function (error) {
+fs.writeFile("write.html", isiKonten, function (error) {
   if (error) throw "Coba lagi masih error";
   console.log("Yeay berhasil");
 });
