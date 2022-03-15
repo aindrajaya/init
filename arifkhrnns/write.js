36b768eba511ba34ff9c4eb380202ef09572f0f6
arifkhrnss <arif.khairunnas@gmail.com>
2022-03-15 19:43:47 +0700

    ubah changed request

--- arifkhrnns/write.js
+++ arifkhrnns/write.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 
-//Penamaan variabel untuk js biasanya pakai camelCase
-const isiKonten = 'Hello world';
+const isiKonten = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test write.js</h1>
+</body>
+</html>
+`;
 
-fs.writeFile('newfile.txt', isiKonten, function(err){
-    if(err) throw err;
-    console.log('File Created')
+fs.writeFileSync('coba.html', isiKonten, function(err) {
+    if (err) throw err;
+    console.log('File Created');
 });
