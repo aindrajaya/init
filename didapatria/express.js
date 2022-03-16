1c746a1f9e3325f96369d56a9c16e26ecd983e06
didapatria <didapatria3@gmail.com>
2022-03-16 21:48:55 +0700

    coba ejs

--- didapatria/express.js
+++ didapatria/express.js
@@ -19,27 +19,34 @@ const html1 =
 </body>
 </html>`;
 
-const html2 =  
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <div style="background-color: blue;">
-    <h1>Hallo Kelas FE JS 7 - Halaman 2</h1>
-  </div>
-</body>
-</html>`;
+// const html2 =  
+// `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div style="background-color: blue;">
+//     <h1>Hallo Kelas FE JS 7 - Halaman 2</h1>
+//   </div>
+// </body>
+// </html>`;
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send(html1);
+  res.render("index");
+});
+app.get("/profile", (req, res) => {
+  const nama = "Dida";
+  const dynamicName = req.query.dynamicName || "Dida";
+  res.render("profile", {nama, dynamicName});
 });
 app.get("/baru", (req, res) => {
-  res.send(html2);
+  res.send(html1);
 });
 
 app.listen(port, () => {
