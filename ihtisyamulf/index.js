f4aaec68a96a01c621d90c3789992d2a01bc9bcd
Asyima37 <ihtisyam376@gmail.com>
2022-05-31 10:55:53 +0700

    update terbaru

--- ihtisyamulf/index.js
+++ ihtisyamulf/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 1447;
+const port = 80;
 
 //let data = "string dari variable";
-let html =
+let html1 =
 `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman 2</title>
 </head>
 <body>
    <div style="background-color: red"> 
@@ -20,6 +20,24 @@ let html =
 </html>
 `
 ;
-app.get("/", (req, res) => res.send(html));
+
+let html2 = 
+`
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman 2</title>
+</head>
+<body>
+   <div style="background-color: blue"> 
+    <h1>Hello Kelas FE JS 7</h1>
+    </div>
+</body>
+</html>
+`;
+app.get("/", (req, res) => res.send(html1));
+app.get("/baru", (req, res) => res.send(html2));
 
 app.listen(port, () => console.log(`This example App listening at http://localhost:${port}`));
