98eb13ce2021bc3ecb9f1b93c5b50090a0dc63eb
Asyima37 <ihtisyam376@gmail.com>
2022-05-31 10:41:54 +0700

    update terbaru

---
+++ ihtisyamulf/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 1447;
+
+//let data = "string dari variable";
+let html =
+`
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <div style="background-color: red"> 
+    <h1>Hello Kelas FE JS 7</h1>
+    </div>
+</body>
+</html>
+`
+;
+app.get("/", (req, res) => res.send(html));
+
+app.listen(port, () => console.log(`This example App listening at http://localhost:${port}`));
